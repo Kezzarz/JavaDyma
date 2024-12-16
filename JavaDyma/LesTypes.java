342ba4ee5ac9a4fd86e429e8d88b795ad5e6f3e5
CannizzaroKevin <skylar74@outlook.fr>
2024-12-16 22:55:57 +0100

    Second push

---
+++ JavaDyma/LesTypes.java
@@ -0,0 +1,26 @@
+public class LesTypes {
+    public static void main(String[] args) {
+        //Les entiers
+        byte octet = 127;
+        short unPeuPlusGrand = 127;
+        int entier = 8432843;
+        long entierLong = 435343566343;
+
+        //Flottants
+        float petitFloat = 42.4f;
+        double grandFloat = 8432.4325;
+
+        //les caractères
+        char uneLettre = 'r';
+
+        //Les booléens
+        boolean uneCondition = true;
+
+        //Les chaînes de caractères
+        String uneChaine = "djiozhiodnoiz";
+
+        //inférence
+        var nimporteQuoi = true;
+
+    }
+}
